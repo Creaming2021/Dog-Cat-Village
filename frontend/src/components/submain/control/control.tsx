7ceda1cd3aff82665371f7e74d219d8a9b581fd4
성진옥 <wlsdhr0831@naver.com>
2021-03-17 03:32:43 +0900

    [feat] Add changing page logic in submain
    Add changing page logic in submain
    type : user, center
    view : main, logIn, join, findPassword
    
    S04P22B106-229

---
+++ frontend/src/components/submain/control/control.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Nav from '../../nav/nav';
+import FindPassword from '../findPassword/findPassword';
+import Join from '../join/join';
+import LogIn from '../logIn/logIn';
+import Main from '../main/main';
+
+const Control = () => {
+    const [view, setView] = useState<'main' | 'logIn' | 'join' | 'findPassword' >('main');
+    const [type, setType] = useState<string>('');
+
+    const goToMain = (): void => {
+        setView('main');
+        setType('');
+    }
+
+    const goToLogIn = (type: string): void  => {
+        setView('logIn');
+        setType(type);
+    }
+
+    const goToJoin = (type: string): void  => {
+        setView('join');
+        setType(type);
+    }
+
+    const goToFindPassword = (type: string): void  => {
+        setView('findPassword');
+        setType(type);
+    }
+
+    return (<>
+        <Nav name="subMain"/>
+        { view === 'main' && 
+            <Main
+                goToLogIn={goToLogIn}/>}
+        { view === 'logIn' && 
+            <LogIn 
+                type={type}
+                goToMain={goToMain}
+                goToJoin={goToJoin}
+                goToFindPassword={goToFindPassword}/>}
+        { view === 'join' && 
+            <Join 
+                type={type}
+                goToLogIn={goToLogIn}/>} 
+        { view === 'findPassword' && 
+            <FindPassword 
+                type={type}
+                goToJoin={goToJoin}
+                goToLogIn={goToLogIn}/>}
+    </>);
+}
+
+export default Control;
