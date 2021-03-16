0f7f5e192bb3a2893aa1c2c0cb5025390877e8a7
성진옥 <wlsdhr0831@naver.com>
2021-03-17 03:30:26 +0900

    [feat] Add route and nav props
    Add route in app.tsx, index.tsx
    Add nav props type in nav.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import styles from './App.module.css';
+import Control from './components/submain/control/control';
 
 function App() {
   return (
     <div className={styles.app}>
-      <h1>마지막 블록체인</h1>
+      <Route path="/" component={Control}/>
+      {/* <Route path="/user" component={User}/>
+      <Route path="/center" component={Center}/> */}
     </div>
   );
 }
