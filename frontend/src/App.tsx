29a33a6b9c7a36afb7a68f431a04563f92c3bfa7
성진옥 <wlsdhr0831@naver.com>
2021-03-16 09:52:59 +0900

    [test] modify project files setting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import styles from './App.module.css';
 
 function App() {
   return (
-    <div className={styles.app__size}>
+    <div className={styles.app}>
       <h1>마지막 블록체인</h1>
     </div>
   );
