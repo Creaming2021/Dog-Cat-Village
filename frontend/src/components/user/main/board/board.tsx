463904d9ec67449bcb8a650a6bac4dea57722623
성진옥 <wlsdhr0831@naver.com>
2021-03-18 04:08:17 +0900

    [docs] Add empty component files of center main category
    just return empty tag
    
    #S04P22B106-165

---
+++ frontend/src/components/user/main/board/board.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import styles from './board.module.css';
+
+const Board = () => {
+    return (
+    <div className={styles.board}>
+        게시판
+    </div>);
+}
+
+export default Board;
