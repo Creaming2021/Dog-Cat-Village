dd2caa8c9646dc90401afc805717b16bf1ef5e81
Jiyoung <jayyyoung321@gmail.com>
2021-03-18 20:50:07 +0900

    [fix] add components to mypage and fix css
    add components to mypage
    fix css of mypage
    
    S04P22B106-246

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './App.module.css';
-import DonationList from './components/donationList/donationList';
-import UserInfo from './components/userInfo/userInfo';
+import MyPage from './components/myPage/myPage';
+
 
 
 
@@ -9,9 +9,7 @@ import UserInfo from './components/userInfo/userInfo';
 function App() {
   return (
     <div className={styles.app}>
-      <h1>마지막 블록체인</h1>
-      <DonationList />
-      <UserInfo />
+      <MyPage />
     </div>
   );
 }
--- frontend/src/components/myPage/myPage.js
+++ frontend/src/components/myPage/myPage.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './myPage.module.css';
 
+import DonatedShelterList from '../donatedShelterList/donatedShelterList';
+import DonationList from '../donationList/donationList';
+import UserInfo from '../userInfo/userInfo';
+import Wallet from '../wallet/wallet';
+
+
 function MyPage() {
+  
   const [userType, setUserType] = useState('user');
 
   return (
-    <div className={`${styles.logIn} ${type === 'user' ? styles.user : styles.shelter}`}>
-
+    <div className={`${styles.mypage} ${userType === 'user' ? styles.user : styles.shelter}`}>
+      <div className={styles.upperbox}>
+        {/* 로고 */}
+        {/* 네브바 */}
+        {/* 이모티콘? 어디로 가는거지..? */}
+      </div>
+      <div className={styles.content}>
+        <div className={styles.leftbox}>
+          <UserInfo className={styles['user-info']} />
+          <Wallet className={styles.wallet} />
+        </div>
+        <DonationList className={styles['donation-list']}/>
+        <DonatedShelterList className={styles['donated-shelter-list']}/>
+      </div>
     </div>
   );
 };
