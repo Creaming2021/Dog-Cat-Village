f2e324ead14ebd99df02168ab923360dba5fcba7
성진옥 <wlsdhr0831@naver.com>
2021-03-23 21:35:36 +0900

    [style] accept code review
    change em to rem
    remove useState, useEffect

--- frontend/src/components/common/common.tsx
+++ frontend/src/components/common/common.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './common.module.css';
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
