33560c2c60366567c0837f01d9d8531777fcf767
성진옥 <wlsdhr0831@naver.com>
2021-03-23 20:19:33 +0900

    [feat] Make common component sets
    Buttons, Images, SearchBox in common components

---
+++ frontend/src/components/common/common.tsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from 'react';
+import styles from './common.module.css';
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type ButtonProps = {
+  value: string,
+  onClick: () => void
+}
+
+export const ButtonLarge = ({ value, onClick }: ButtonProps) => {
+  return <button
+    className={styles['btn-large']}
+    onClick={onClick}>{value}</button>;
+}
+
+export const ButtonMedium = ({ value, onClick }: ButtonProps) => {
+  return <button
+    className={styles['btn-medium']}
+    onClick={onClick}>{value}</button>;
+}
+
+export const ButtonSmall = ({ value, onClick }: ButtonProps) => {
+  return <button
+    className={styles['btn-small']}
+    onClick={onClick}>{value}</button>;
+}
+
+type ImageProps = {
+  src: string,
+  alt: string,
+}
+
+export const ImageLarge = ({ src, alt }: ImageProps) => {
+  return <img
+    className={styles['img-large']}
+    src={src} alt={alt} />;
+}
+
+export const ImageMedium = ({ src, alt }: ImageProps) => {
+  return <img
+    className={styles['img-medium']}
+    src={src} alt={alt} />;
+}
+
+export const ImageSmall = ({ src, alt }: ImageProps) => {
+  return <img
+    className={styles['img-small']}
+    src={src} alt={alt} />;
+}
+
+export const ImageXsmall = ({ src, alt }: ImageProps) => {
+  return <img
+    className={styles['img-xsmall']}
+    src={src} alt={alt} />;
+}
+
+
+interface optionType {
+  value: string,
+  option: string,
+}
+
+interface selectType {
+  name: string,
+  value?: string,
+  options: optionType[],
+}
+
+type SearchProps = {
+  selectList: selectType[],
+  selectValue: string[],
+  inputName: string,
+  inputValue: string,
+  onSearch: () => void,
+  onChange: (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>)
+    => void,
+  placeholder: string,
+  size: string,
+}
+
+export const Search = ({ selectList, selectValue, inputName, inputValue, onSearch, onChange, placeholder, size }: SearchProps) => {
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.nativeEvent.key === "Enter") {
+      onSearch();
+    }
+  }
+
+  return (
+    <div className={styles['search-container']}>
+      { selectList.map((select, index) =>
+        <select
+          key={index}
+          className={styles['search-option']}
+          name={select.name}
+          value={selectValue[index]}
+          onChange={onChange}>
+          {select.options.map((option, index) =>
+            <option
+              key={index}
+              value={option.value}>{option.option}</option>)}
+        </select>
+      )}
+      <input 
+        className={styles[`${size}`]}
+        placeholder={placeholder}
+        name={inputName}
+        value={inputValue}
+        onChange={onChange}
+        onKeyDown={onKeyDown} />
+      <FontAwesomeIcon
+        className={styles['search-icon']}
+        icon={faSearch}
+        onClick={onSearch} />
+    </div>);
+}
