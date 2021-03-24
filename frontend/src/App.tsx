29f86f75ca9ce0c6ed2de697e81afe902453f070
Jiyoung <jayyyoung321@gmail.com>
2021-03-24 14:21:35 +0900

    [feat] add router of shelterListPage
    add router function of shelterListPage in app.tsx
    
    #S04P23B106-146

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import styles from './App.module.css';
 import SubMain from './components/user/main/subMain/subMain';
 import UserContainer from './containers/user_container';
 import MyPage from './components/myPage/myPage';
+import shelterListPage from './components/shelterListPage/shelterListPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" component={UserContainer} exact/>
       <Route path="/user" component={MyPage} exact/>
       <Route path="/shelter" component={SubMain} exact/>
+      <Route path="/shelter/list" component={shelterListPage} exact/>
       {/* <Route path="/user" component={User}/>
       <Route path="/center" component={Center}/> */}
     </div>
