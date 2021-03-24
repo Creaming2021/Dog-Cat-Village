5585c297eeeea0c6f2bdea01cafabb09fdcb81ee
성진옥 <wlsdhr0831@naver.com>
2021-03-24 20:43:18 +0900

    [feat] delete board, notice button and logic
    delete button about board, notice
    
    #S04P23B106-35

--- frontend/src/components/user/main/subMain/subMain.tsx
+++ frontend/src/components/user/main/subMain/subMain.tsx
@@ -9,26 +9,30 @@ import MainCategory from '../mainCategory/mainCategory';
 import Notice from '../notice/notice';
 import styles from './subMain.module.css';
 
-const SubMain = () => {
-    type CategoryType = 'home' | 'notice' | 'board' | 'animal' | 'chatting' | 'donation';  
-    
-    const [category, setCategory] = useState<CategoryType>('animal');
+const SubMain = () => {    
+    const [category, setCategory] = useState<string>('animal');
 
-    const onChangeCategory = (category: CategoryType): void => {
+    const onChangeCategory = (category: string): void => {
         setCategory(category);
+    }
+
+    const userInfo = {
+        name: "동렬 아저씨의 동물 농장",
+        imageUrl: "https://img2.sbs.co.kr/img/sbs_cms/WE/2020/07/10/WE61973057_ori.jpg",
+        siteUrl: "www.naver.com",
+        phoneNumber: "01012345678",
+        email: "ssafy@ssafy.com",
+        introduction: "안녕하세요 동아동입니다~ 많은 관심과 후원 감사합니다! 하는 등의 소개글"
     }
 
     return (
     <div className={styles['sub-main']}>
         <Nav name="centerMainPage"/>
         <MainCategory
+            userInfo={userInfo}
             onChangeCategory={onChangeCategory}/>
         {category === 'home' &&
             <Home type='user'/>}
-        {category === 'notice' &&
-            <Notice/>}
-        {category === 'board' &&
-            <Board/>}
         {category === 'animal' &&
             <Animal type='center'/>}
         {category === 'chatting' &&
