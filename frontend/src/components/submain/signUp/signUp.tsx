e60048da6dfe36502862594af8f57d8b39318096
성진옥 <wlsdhr0831@naver.com>
2021-03-24 19:14:19 +0900

    [feat] Add description text in sign up
    add description text in sign up (개인, 보호소)
    
    #S04P23B106-151

--- frontend/src/components/submain/signUp/signUp.tsx
+++ frontend/src/components/submain/signUp/signUp.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import styles from "./signUp.module.css";
 import commons from "../../common/common.module.css";
 import { SignUpInputType } from "../../../interface/user";
-import { ButtonLarge } from "../../common/common";
+import SignUpForm from './signUpForm';
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type SignUpProps = {
   type: string;
@@ -94,7 +96,7 @@ const SignUp = ({
   };
 
   // 중복 체크 하는 함수
-  const onCheckNickname = (): void => {
+  const onCheckDuplicated = (): void => {
     if(type === 'user' && checkNickname){
       setInputState({ ...inputState, duplicated: checkNickname()});
     }else if(type === 'center' && checkName) {
@@ -112,171 +114,45 @@ const SignUp = ({
   }
 
   return (
-    <div
-      className={`${styles.join} 
-                    ${ type === "user"
-                      ? commons["bg-gradient-yellow-green"]
-                      : commons["bg-gradient-green-blue"]}`}>
-      <div className={`${commons["box-white"]} ${styles.box}`}>
-        <button
-          className={`${commons["btn-text"]} 
-                      ${commons["text-right"]} 
-                      ${styles.button}
-                      ${type === "user"
-                        ? commons["text-yellow"]
-                        : commons["text-blue"]}`}
-          onClick={() => goToLogIn(type)}>
-          로그인 하러 가기</button>
-        <div className={styles.form}>
-          <input
-            className={`${commons["input-small"]} 
-                        ${styles.input}
-                        ${type === "user"
-                          ? commons["border-yellow"]
-                          : commons["border-blue"]}`}
-            type="text"
-            name="emailId"
-            value={emailId}
-            onChange={onChangeSignUp}
-            onKeyDown={onKeyDown}
-            placeholder="E-mail"/>
-          <div className={styles["icon-at"]}>@</div>
-          <input
-            className={`${commons["input-small"]} 
-                        ${styles.input}
-                        ${type === "user"
-                          ? commons["border-yellow"]
-                          : commons["border-blue"]}`}
-            type="text"
-            name="emailSite"
-            value={emailSite}
-            onChange={onChangeSignUp}
-            onKeyDown={onKeyDown}
-            placeholder="xxx.xxx"/><br/>
-          {inputState.email || (
-            <p className={styles['text-xsmall-light']}>
-              이메일을 입력하세요.</p>)}
-          <input
-            className={`${commons["input-large"]} 
-                        ${styles.input}
-                        ${type === "user"
-                          ? commons["border-yellow"]
-                          : commons["border-blue"]}`}
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChangeSignUp}
-            onKeyDown={onKeyDown}
-            placeholder="PW"/><br />
-          {inputState.password || (
-            <p className={styles['text-xsmall-light']}>
-              비밀번호는 숫자, 영문자, 특수문자 조합으로 8자리 이상 이여야 합니다.</p>)}
-          <input
-            className={`${commons["input-large"]} 
-                        ${styles.input}
-                        ${type === "user"
-                          ? commons["border-yellow"]
-                          : commons["border-blue"]}`}
-            type="password"
-            name="passwordConfirm"
-            value={passwordConfirm}
-            onChange={onChangeSignUp}
-            placeholder="PW CONFIRM"/><br />
-          {inputState.passwordConfirm || (
-            <p className={styles['text-xsmall-light']}>비밀번호가 일치하지 않습니다.</p>)}
-          {type === "user" 
-          ? (<>
-              <input
-                className={`${styles["input-nickname"]} 
-                            ${styles.input}
-                            ${type === "user"
-                              ? commons["border-yellow"]
-                              : commons["border-blue"]}`}
-                type="text"
-                name="nickname"
-                value={nickname}
-                onChange={onChangeSignUp}
-                onKeyDown={onKeyDown}
-                placeholder="NICKNAME"/>
-              <button
-                className={`${styles['btn-check']} 
-                          ${type === "user"
-                            ? commons["bg-white-yellow"]
-                            : commons["bg-white-blue"]}`}
-                name="duplication"
-                onClick={onCheckNickname}>중복 확인</button><br />
-            </>) 
-          : (<>
-              <input
-                className={`${styles["input-nickname"]} 
-                            ${styles.input}
-                            ${type === "user"
-                              ? commons["border-yellow"]
-                              : commons["border-blue"]}`}
-                type="text"
-                name="name"
-                value={name}
-                onChange={onChangeSignUp}
-                onKeyDown={onKeyDown}
-                placeholder="단체명"/>
-              <button
-                className={`${styles['btn-check']} 
-                          ${type === "user"
-                            ? commons["bg-white-yellow"]
-                            : commons["bg-white-blue"]}`}
-                name="duplication"
-                onClick={onCheckNickname}>중복 확인</button><br />
-            </>)}
-          {inputState.duplicated || (
-            <p className={styles['text-xsmall-light']}>중복 확인을 하세요.</p>)}
-          <input
-            className={`${commons["input-xsmall"]} 
-                        ${styles.input}
-                        ${type === "user"
-                          ? commons["border-yellow"]
-                          : commons["border-blue"]}`}
-            type="text"
-            name="phoneNumber1"
-            value={phoneNumber1}
-            onChange={onChangeSignUp}
-            onKeyDown={onKeyDown}
-            placeholder="010"/>
-          -
-          <input
-            className={`${commons["input-xsmall"]} 
-                        ${styles.input}
-                        ${type === "user"
-                          ? commons["border-yellow"]
-                          : commons["border-blue"]}`}
-            type="text"
-            name="phoneNumber2"
-            value={phoneNumber2}
-            onChange={onChangeSignUp}
-            onKeyDown={onKeyDown}
-            placeholder="1234"
-          />
-          -
-          <input
-            className={`${commons["input-xsmall"]} 
-                        ${styles.input}
-                        ${type === "user"
-                          ? commons["border-yellow"]
-                          : commons["border-blue"]}`}
-            type="text"
-            name="phoneNumber3"
-            value={phoneNumber3}
-            onChange={onChangeSignUp}
-            onKeyDown={onKeyDown}
-            placeholder="5678"/><br />
-          {inputState.phoneNumber || (
-            <p className={styles['text-xsmall-light']}>번호를 확인하세요.</p>)}
-          <ButtonLarge
-            value="SIGN IN"
-            onClick={onSubmitSignUp}
-            buttonColor={type === "user" ? "bg-yellow" : "bg-blue"}/>
-        </div>
-      </div>
+  <div className={`${styles['sign-up-container']} 
+                ${ type === "user"
+                  ? commons["bg-gradient-yellow-green"]
+                  : commons["bg-gradient-green-blue"]}`}>
+    <div className={`${commons["box-white"]} ${styles['left-container']}`}>
+      <button
+        className={`${commons["btn-text"]} 
+                    ${commons["text-right"]} 
+                    ${styles.button}
+                    ${type === "user"
+                      ? commons["text-yellow"]
+                      : commons["text-blue"]}`}
+        onClick={() => goToLogIn(type)}>
+        로그인 하러 가기</button>
+      <SignUpForm
+        type={type}
+        signUpInput={signUpInput}
+        inputState={inputState}
+        onChangeSignUp={onChangeSignUp}
+        onKeyDown={onKeyDown}
+        onCheckDuplicated={onCheckDuplicated}
+        onSubmitSignUp={onSubmitSignUp} />
     </div>
+    <div className={`${styles['right-container']}`}>
+      <div>
+        <div className={`${commons['text-medium-bold']}`}>멍냥이 빌리지에 오신 걸</div>
+        <p className={`${commons['text-large']}`}>환영합니다!</p>
+      </div>
+      <div>
+        <div className={`${commons['text-small-light']}`}><FontAwesomeIcon icon={faPaw}/> 즐겁게</div>
+        <div className={`${commons['text-small-light']}`}><FontAwesomeIcon icon={faPaw}/> 편리하게</div>
+        <div className={`${commons['text-small-light']}`}><FontAwesomeIcon icon={faPaw}/> 안전하게</div>
+        <div className={`${commons['text-small-light']}`}><FontAwesomeIcon icon={faPaw}/> 간편하게</div>
+      </div>
+      { type === 'user'
+        ? <p className={`${commons['text-medium-light']}`}>후원하세요!</p>
+        : <p className={`${commons['text-medium-light']}`}>시작하세요!</p> }
+    </div>
+  </div>
   );
 };
 
---
+++ frontend/src/components/submain/signUp/signUpForm.tsx
@@ -0,0 +1,174 @@
+import React from "react";
+import styles from "./signUp.module.css";
+import commons from "../../common/common.module.css";
+import { SignUpInputType } from "../../../interface/user";
+import { ButtonLarge } from "../../common/common";
+
+type SignUpFormProps = {
+  type: string;
+  signUpInput: SignUpInputType;
+  inputState: {
+    email: boolean, 
+    password: boolean, 
+    passwordConfirm: boolean, 
+    duplicated: boolean, 
+    phoneNumber: boolean
+  },
+  onChangeSignUp: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onCheckDuplicated: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onSubmitSignUp: () => void;
+}
+
+const SignUpForm = ({
+  type,
+  signUpInput,
+  inputState,
+  onChangeSignUp,
+  onKeyDown,
+  onCheckDuplicated,
+  onSubmitSignUp,
+}: SignUpFormProps) => {
+
+  // 회원가입폼 구성하는 속성들 비구조화 할당
+  const {
+    emailId,
+    emailSite,
+    nickname,
+    password,
+    passwordConfirm,
+    phoneNumber1,
+    phoneNumber2,
+    phoneNumber3,
+    name,
+  } = signUpInput;
+
+  return (
+    <div className={styles.form}>
+    <input
+      className={`${commons["input-small"]} 
+                  ${styles.input}
+                  ${type === "user"
+                    ? commons["border-yellow"]
+                    : commons["border-blue"]}`}
+      type="text"
+      name="emailId"
+      value={emailId}
+      onChange={onChangeSignUp}
+      onKeyDown={onKeyDown}
+      placeholder="E-mail"/>
+    <div className={styles["icon-at"]}>@</div>
+    <input
+      className={`${commons["input-small"]} 
+                  ${styles.input}
+                  ${type === "user"
+                    ? commons["border-yellow"]
+                    : commons["border-blue"]}`}
+      type="text"
+      name="emailSite"
+      value={emailSite}
+      onChange={onChangeSignUp}
+      onKeyDown={onKeyDown}
+      placeholder="xxx.xxx"/><br/>
+    {inputState.email || (
+      <p className={styles['text-xsmall-light']}>
+        이메일을 입력하세요.</p>)}
+    <input
+      className={`${commons["input-large"]} 
+                  ${styles.input}
+                  ${type === "user"
+                    ? commons["border-yellow"]
+                    : commons["border-blue"]}`}
+      type="password"
+      name="password"
+      value={password}
+      onChange={onChangeSignUp}
+      onKeyDown={onKeyDown}
+      placeholder="PW"/><br />
+    {inputState.password || (
+      <p className={styles['text-xsmall-light']}>
+        비밀번호는 숫자, 영문자, 특수문자 조합으로 8자리 이상 이여야 합니다.</p>)}
+    <input
+      className={`${commons["input-large"]} 
+                  ${styles.input}
+                  ${type === "user"
+                    ? commons["border-yellow"]
+                    : commons["border-blue"]}`}
+      type="password"
+      name="passwordConfirm"
+      value={passwordConfirm}
+      onChange={onChangeSignUp}
+      placeholder="PW CONFIRM"/><br />
+    {inputState.passwordConfirm || (
+      <p className={styles['text-xsmall-light']}>비밀번호가 일치하지 않습니다.</p>)}
+    <input
+      className={`${styles["input-nickname"]} 
+                  ${styles.input}
+                  ${type === "user"
+                    ? commons["border-yellow"]
+                    : commons["border-blue"]}`}
+      type="text"
+      name={type === 'user'? "nickname" : "name"}
+      value={type === 'user'? nickname : name}
+      onChange={onChangeSignUp}
+      onKeyDown={onKeyDown}
+      placeholder={type === 'user'? "NICKNAME" : "단체명"}/>
+    <button
+      className={`${styles['btn-check']} 
+                ${type === "user"
+                  ? commons["bg-white-yellow"]
+                  : commons["bg-white-blue"]}`}
+      name="duplication"
+      onClick={onCheckDuplicated}>중복 확인</button><br />
+    {inputState.duplicated || (
+      <p className={styles['text-xsmall-light']}>중복 확인을 하세요.</p>)}
+    <input
+      className={`${commons["input-xsmall"]} 
+                  ${styles.input}
+                  ${type === "user"
+                    ? commons["border-yellow"]
+                    : commons["border-blue"]}`}
+      type="text"
+      name="phoneNumber1"
+      value={phoneNumber1}
+      onChange={onChangeSignUp}
+      onKeyDown={onKeyDown}
+      placeholder="010"/>
+    -
+    <input
+      className={`${commons["input-xsmall"]} 
+                  ${styles.input}
+                  ${type === "user"
+                    ? commons["border-yellow"]
+                    : commons["border-blue"]}`}
+      type="text"
+      name="phoneNumber2"
+      value={phoneNumber2}
+      onChange={onChangeSignUp}
+      onKeyDown={onKeyDown}
+      placeholder="1234"
+    />
+    -
+    <input
+      className={`${commons["input-xsmall"]} 
+                  ${styles.input}
+                  ${type === "user"
+                    ? commons["border-yellow"]
+                    : commons["border-blue"]}`}
+      type="text"
+      name="phoneNumber3"
+      value={phoneNumber3}
+      onChange={onChangeSignUp}
+      onKeyDown={onKeyDown}
+      placeholder="5678"/><br />
+    {inputState.phoneNumber || (
+      <p className={styles['text-xsmall-light']}>번호를 확인하세요.</p>)}
+    <ButtonLarge
+      value="SIGN IN"
+      onClick={onSubmitSignUp}
+      buttonColor={type === "user" ? "bg-yellow" : "bg-blue"}/>
+  </div> 
+  );
+}
+
+export default SignUpForm;
