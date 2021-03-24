31ade8b028929c1a518fccbc7d64c900881bc42f
Jiyoung <jayyyoung321@gmail.com>
2021-03-24 17:56:31 +0900

    [feat] add streamingListPage markup
    add markup of streamingListItem.js
    add markup of stramingListPage.js
    
    #S04P23B106-149

--- frontend/src/components/streamingListPage/streamingListItem.js
+++ frontend/src/components/streamingListPage/streamingListItem.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import styles from './streamingListItem.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
-const StreamingListItem = () => {
+const StreamingListItem = ({data}) => {
   return(
-    <>
-    </>
+    <div className={styles['streaming-container']}>
+      <div className={styles['img-container']}>
+        <img src={data.img} className={styles['streaming-img']} />
+      </div>
+      <div className={styles['straming-discription-container']}>
+        <div className={styles['streaming-name']}>
+          {data.streamingName}
+        </div>
+        <div className={styles['discription-inner-container']}>
+          <div className={styles['shelter-name']}>
+            {data.shelterName}
+          </div>
+          <div className={styles['viewer-container']}>
+            <FontAwesomeIcon icon={faUserCircle} className={styles['user-icon']}/>
+            <div className={styles['viewer-num']}>{data.viewer}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/components/streamingListPage/streamingListPage.js
+++ frontend/src/components/streamingListPage/streamingListPage.js
@@ -1,9 +1,81 @@
 import React from 'react';
 import styles from './streamingListPage.module.css';
+import StreamingListItem from './streamingListItem';
+
+
+const streamingList = [
+  {
+    img: "https://picsum.photos/440/250",
+    streamingName: "후원은 사랑입니다",
+    shelterName: "동렬 아저씨의 동물농장",
+    viewer: "123" 
+  },
+  {
+    img: "https://picsum.photos/440/251",
+    streamingName: "후원은 사랑입니다",
+    shelterName: "해피 도그",
+    viewer: "1233" 
+  },
+  {
+    img: "https://picsum.photos/440/252",
+    streamingName: "후원은 사랑입니다",
+    shelterName: "캣 하우스",
+    viewer: "7544"
+  },
+  {
+    img: "https://picsum.photos/441/250",
+    streamingName: "후원은 사랑입니다",
+    shelterName: "몽키 몽키",
+    viewer: "75123"
+  },
+  {
+    img: "https://picsum.photos/441/251",
+    streamingName: "후원은 사랑입니다",
+    shelterName: "돼지 피그",
+    viewer: "286123"
+  },
+  {
+    img: "https://picsum.photos/441/252",
+    streamingName: "후원은 사랑입니다",
+    shelterName: "냥냥이 커넥트",
+    viewer: "235"
+  },
+  {
+    img: "https://picsum.photos/442/250",
+    streamingName: "후원은 사랑입니다",
+    shelterName: "고양이는 귀엽다",
+    viewer: "6123"
+  },
+  {
+    img: "https://picsum.photos/442/251",
+    streamingName: "후원은 사랑입니다",
+    shelterName: "매니 도네이션",
+    viewer: "889"
+  },
+]
 
 const StreamingListPage = () => {
   return (
     <>
+      <div>
+        {/* 로고 */}
+        {/*  네브바 */}
+        {/* 마이페이지 아이콘  */}
+      </div>
+      <div>
+        {/* 검색 기능 */}
+      </div>
+      <div className={styles['main-container']}>
+        {
+          streamingList.map((data) => {
+            return (
+              <div className={styles['streaming-item-container']}>
+                <StreamingListItem data={data} />
+              </div>
+            )
+          })
+        }
+      </div>
     </>
   );
 };
