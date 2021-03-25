e9bfeb8aa7cea8be5c521659ac602eabb9de6ee5
Jiyoung <jayyyoung321@gmail.com>
2021-03-25 23:21:33 +0900

    [feat] add board component in user_mainpage
    - add js of board fadein, fadeout component
    - add css of board fadein, fadeout component
    
    #S04P23B106-166

--- frontend/src/components/userMainPage/userMainPage.js
+++ frontend/src/components/userMainPage/userMainPage.js
@@ -1,28 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './userMainPage.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faIdBadge, faVideo, faCoins,  faDog } from '@fortawesome/free-solid-svg-icons';
 
+const coin = [0,0,0]
+const animal = [0,0,0]
+
+
+const todayTotalDonation = () => {
+  return (
+    <div className={styles['main-part']}>
+      <h1 className={styles['main-text']}>오늘 기부된 총 금액</h1>
+      {
+        coin.map(number => {
+          return (
+            <div className={`${styles['number-box']} ${styles.yellow}`}>
+              <h1 className={styles['number-text']}>{number}</h1>
+            </div>
+          );
+        })
+      }
+      <h1 className={styles['main-text']}>코인</h1>
+    </div>
+  );
+}
+
+const todayAdoptedAnimalCount = () => {
+  return (
+    <div className={styles['main-part']}>
+      <h1 className={styles['main-text']}>오늘 입양된 동물 수</h1>
+      {
+        animal.map(number => {
+          return (
+            <div className={`${styles['number-box']} ${styles.blue}`}>
+              <h1 className={styles['number-text']}>{number}</h1>
+            </div>
+          );
+        })
+      }
+      
+      <h1 className={styles['main-text']}>마리</h1>
+    </div>
+  );
+}
+
+
+
 
 const UserMainPage = () => {
+  const [boardToggle, setBoardToggle] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setBoardToggle(!boardToggle);
+    }, 10000);
+  }, [boardToggle]);
+
   return (
     <>
       <div className={styles['upper-part']}>
         {/* 로고 */}
         <FontAwesomeIcon icon={faIdBadge} className={styles['idBadge-icon']} />
       </div> 
-      <div className={styles['main-part']}>
-        <h1 className={styles['main-text1']}>오늘 기부된 총 금액</h1>
-        <div className={styles['number-box1']}>
-          <h1 className={styles['number-text1']}>3</h1>
-        </div>
-        <div className={styles['number-box2']}>
-          <h1 className={styles['number-text2']}>2</h1>
-        </div>
-        <div className={styles['number-box3']}>
-          <h1 className={styles['number-text3']}>0</h1>
-        </div>
-        <h1 className={styles['main-text2']}>코인</h1>
+      <div className={boardToggle ? styles.displayNone : styles.display }>
+        {todayTotalDonation()}
+      </div>
+      <div className={boardToggle ? styles.display :  styles.displayNone }>
+        {todayAdoptedAnimalCount()}
       </div>
       <div className={styles['lower-part']}>
         <div className={styles['vod-container']}>
