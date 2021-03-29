6f0ecfddcd568229c606122d894d83b7d5febd45
성진옥 <wlsdhr0831@naver.com>
2021-03-29 11:33:50 +0900

    [style] modify code after code review
    [style] modify code after code review
    [style] modify indent

--- frontend/src/interface/animal.ts
+++ frontend/src/interface/animal.ts
@@ -1,34 +1,34 @@
 export interface AnimalBasicType {
-	id: number;
-	name: string;
-	imageUrl: string;
-	sex: string;
-	breedType: string;
+  id: number;
+  name: string;
+  imageUrl: string;
+  sex: string;
+  breedType: string;
 }
 
 export interface AnimalExtraType {
-	weight: string; 
-	breed: string;
-	personality: string;
-	neuter: string; 
-	condition: string;
+  weight: string;
+  breed: string;
+  personality: string;
+  neuter: string;
+  condition: string;
 }
 
 export interface AnimalListType extends AnimalBasicType {
-	birthday: string;
-	age: string;
+  birthday: string;
+  age: string;
 }
 
 export interface AnimalDetailType extends AnimalListType, AnimalExtraType {
-	shelterId: number;
+  shelterId: number;
 }
 
 export interface AnimalInputType extends AnimalBasicType, AnimalExtraType {
-	year: string;
-	month: string;
-	date: string;
+  year: string;
+  month: string;
+  date: string;
 }
 
 export interface AnimalEditType extends AnimalBasicType, AnimalExtraType {
-	birthday: string;
+  birthday: string;
 }
