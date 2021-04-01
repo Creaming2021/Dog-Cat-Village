67dca490249e9203fa8e21e83728336f3cf18de8
김다윤 <ekdbs628@gmail.com>
2021-04-01 18:04:47 +0900

    [fix] Add import

--- backend/src/main/java/donation/pet/config/RedisConfig.java
+++ backend/src/main/java/donation/pet/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package donation.pet.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
