df9ab395cf86b29a97f49edfcbe447c4c400fbd7
Jiyoung <jayyyoung321@gmail.com>
2021-04-06 01:50:13 +0900

    [feat] add consumer api logic
    add consumer api logic
    
    #S04P23B106-292

--- frontend/src/modules/consumer.js
+++ frontend/src/modules/consumer.js
@@ -1,28 +1,34 @@
 // 해당 유저 정보 가져오기
-import { basic } from '../service/instance';
+import { basic, image } from '../service/instance';
+
 
 const GET_CONSUMER_INFO = "consumer/GET_CONSUMER_INFO";
 const PUT_CONSUMER_INFO = "consumer/PUT_CONSUMER_INFO";
 const POST_CONSUMER_PROFILE_IMG = "consumer/POST_CONSUMER_PROFILE_IMG";
 
 export const getUserInfo = (id) => async dispatch => {
-  const response = await basic.get(`/consumers/${id}`)
+  const response = await basic.get(`/consumers/${id}`);
   dispatch({
     type: GET_CONSUMER_INFO,
     payload: response.data,
   });
 };
 
-export const putUserInfo = () => {
-  return {
+export const putUserInfo = (data) => async dispatch => {
+  const response = await basic.put(`/consumers/${data.id}`, data);
+  dispatch({
     type: PUT_CONSUMER_INFO,
-  }
+    payload: response.data,
+  });
 };
 
-export const postUserProfileImg = () => {
-  return {
+export const postUserProfileImg = (data) => async dispatch => {
+  console.log(data);
+  const response = await image.post(`/consumers/${data.id}/image`, data.formData);
+  console.log(response);
+  dispatch ({
     type: POST_CONSUMER_PROFILE_IMG,
-  }
+  });
 };
 
 
@@ -44,12 +50,15 @@ export const consumer = (state=initialState, action) => {
       };
     
     case PUT_CONSUMER_INFO:
-      return state;
+      return {
+        ...state,
+        ...action.payload
+      };
     
     case POST_CONSUMER_PROFILE_IMG:
       return state;
 
     default:
       return state;
-  }
-}
+  };
+};
