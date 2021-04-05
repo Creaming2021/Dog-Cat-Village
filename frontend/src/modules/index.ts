6dec31792fe494d74969698bb3a3e43b6b8696a8
성진옥 <wlsdhr0831@naver.com>
2021-04-05 00:56:06 +0000

    [merge] Feature/fe/adopt

--- frontend/src/modules/index.ts
+++ frontend/src/modules/index.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
-import member, { memberSaga } from './member';
-import test, { githubSaga } from './test';
 import { all } from 'redux-saga/effects';
+import test, { githubSaga } from './test';
+import member, { memberSaga } from './member';
+import adopt, { adoptSaga } from './adopt';
 
 // 여러 리듀서를 합쳐서 rootReducer에 선언
 const rootReducer = combineReducers({
   member,
-  test
+  test,
+  adopt,
 });
 
 export default rootReducer;
@@ -15,5 +17,5 @@ export type RootState = ReturnType<typeof rootReducer>;
 
 // 루트 사가를 만들어서 내보내주세요.
 export function* rootSaga() {
-  yield all([memberSaga(), githubSaga()]);
+  yield all([memberSaga(), githubSaga(), adoptSaga()]);
 }
