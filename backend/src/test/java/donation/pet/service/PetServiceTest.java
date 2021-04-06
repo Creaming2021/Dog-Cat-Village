6931bd4b612e3d653eb7130086d1f82eb32fdfd2
차민석 <51734158+meloncha@users.noreply.github.com>
2021-04-07 03:37:17 +0900

    [fix] except deleted pets

--- backend/src/test/java/donation/pet/service/PetServiceTest.java
+++ backend/src/test/java/donation/pet/service/PetServiceTest.java
@@ -3,10 +3,7 @@ package donation.pet.service;
 import donation.pet.domain.etc.Sex;
 import donation.pet.domain.member.shelter.Shelter;
 import donation.pet.domain.member.shelter.ShelterRepository;
-import donation.pet.domain.pet.BreedType;
-import donation.pet.domain.pet.Neuter;
-import donation.pet.domain.pet.Pet;
-import donation.pet.domain.pet.PetRepository;
+import donation.pet.domain.pet.*;
 import donation.pet.dto.pet.PetRequestDto;
 import donation.pet.dto.pet.PetSimpleDto;
 import donation.pet.exception.BaseException;
@@ -40,22 +37,40 @@ class PetServiceTest {
     @Autowired
     ModelMapper modelMapper;
 
-    @BeforeEach
-    public void before() {
-//        Shelter shelter = Shelter.builder().name("보호소").build();
-        Shelter shelter = shelterRepository.findById(3L)
-                .orElseThrow(() -> new BaseException(ErrorCode.SHELTER_NOT_EXIST));
-        for (int i = 0; i < 10; i++) {
-            PetRequestDto dto = PetRequestDto.builder()
-                    .shelterId(shelter.getId())
-                    .name("쿠로").breed("웰시코기").weight(13.4f)
-                    .birthday("20180207")
-                    .breedType(BreedType.DOG).personality("식탐이 강함").condition("돼지")
-                    .sex(Sex.MALE).neuter(Neuter.YES).build();
-            Pet pet = Pet.createPet(dto, shelter);
-            petRepository.save(pet);
-        }
-        shelterRepository.save(shelter);
+//    @BeforeEach
+//    public void before() {
+////        Shelter shelter = Shelter.builder().name("보호소").build();
+//        Shelter shelter = shelterRepository.findById(3L)
+//                .orElseThrow(() -> new BaseException(ErrorCode.SHELTER_NOT_EXIST));
+//        for (int i = 0; i < 10; i++) {
+//            PetRequestDto dto = PetRequestDto.builder()
+//                    .shelterId(shelter.getId())
+//                    .name("쿠로").breed("웰시코기").weight(13.4f)
+//                    .birthday("20180207")
+//                    .breedType(BreedType.DOG).personality("식탐이 강함").condition("돼지")
+//                    .sex(Sex.MALE).neuter(Neuter.YES).build();
+//            Pet pet = Pet.createPet(dto, shelter);
+//            petRepository.save(pet);
+//        }
+//        shelterRepository.save(shelter);
+//    }
+
+    @Test
+    public void 펫_삭제_개수_확인() throws Exception {
+        // given
+        long deleteCount = petRepository.findAll().stream()
+                .filter(pet -> pet.getAdoptStatus() == AdoptStatus.DELETE).count();
+
+        // when
+        long allCount = petRepository.count();
+        int size = petService.getPetAll().size();
+
+        // then
+        System.out.println("size = " + size);
+        System.out.println("allCount = " + allCount);
+        System.out.println("DELETE 갯수: " + deleteCount);
+
+        assertThat(size).isEqualTo(allCount - deleteCount);
     }
 
     @Test
