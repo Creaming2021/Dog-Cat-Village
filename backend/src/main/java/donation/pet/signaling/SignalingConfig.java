2094d4a31e8f2718a80877f1e375846f84e57c6a
차민석 <51734158+meloncha@users.noreply.github.com>
2021-04-08 00:56:12 +0900

    [fix] revert

--- backend/src/main/java/donation/pet/signaling/SignalingConfig.java
+++ backend/src/main/java/donation/pet/signaling/SignalingConfig.java
@@ -1,34 +1,34 @@
-package donation.pet.signaling;
-
-import org.kurento.client.KurentoClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
-
-@Configuration
-public class SignalingConfig implements WebSocketConfigurer {
-
+//package donation.pet.signaling;
+//
+//import org.kurento.client.KurentoClient;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+//
+//@Configuration
+//public class SignalingConfig implements WebSocketConfigurer {
+//
+////    @Bean
+////    public SignalingHandler signalingHandler() {
+////        return new SignalingHandler();
+////    }
+//
 //    @Bean
-//    public SignalingHandler signalingHandler() {
-//        return new SignalingHandler();
+//    public KurentoClient kurentoClient() {
+//        return KurentoClient.create("ws://0.0.0.0:8888/kurento");
 //    }
-
-    @Bean
-    public KurentoClient kurentoClient() {
-        return KurentoClient.create("ws://0.0.0.0:8888/kurento");
-    }
-
-    @Bean
-    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxTextMessageBufferSize(32768);
-        return container;
-    }
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(signalingHandler(), "/live").setAllowedOrigins("*");
-    }
-}
+//
+//    @Bean
+//    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+//        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+//        container.setMaxTextMessageBufferSize(32768);
+//        return container;
+//    }
+//
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+////        registry.addHandler(signalingHandler(), "/live").setAllowedOrigins("*");
+//    }
+//}
