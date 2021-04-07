5b61a9fa8ad5d272a34e9de89127b52b78b32290
성진옥 <wlsdhr0831@naver.com>
2021-04-07 21:42:01 +0900

    [fix] reload page data persist(redux-persist)

--- frontend/src/modules/index.ts
+++ frontend/src/modules/index.ts
@@ -1,4 +1,6 @@
 import { combineReducers } from "redux";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import member, { memberSaga } from './member';
 import chat, { chatSaga } from './chat';
 import blockchain, { blockchainSaga } from './blockchain';
@@ -7,6 +9,12 @@ import adopt, { adoptSaga } from './adopt';
 import { all } from 'redux-saga/effects';
 import { consumer } from './consumer';
 import { shelter } from './shelter';
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['member'],
+}
 
 // 여러 리듀서를 합쳐서 rootReducer에 선언
 const rootReducer = combineReducers({
@@ -19,11 +27,11 @@ const rootReducer = combineReducers({
   shelter,
 });
 
-export default rootReducer;
-
 export type RootState = ReturnType<typeof rootReducer>;
 
 // 루트 사가를 만들어서 내보내주세요.
 export function* rootSaga() {
   yield all([memberSaga(), chatSaga(), petSaga(), blockchainSaga(), adoptSaga()]);
 }
+
+export default persistReducer(persistConfig, rootReducer);
