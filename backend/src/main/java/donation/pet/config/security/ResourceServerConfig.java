630550480931ebc1472b4c6704f15c3ca8b26c69
차민석 <51734158+meloncha@users.noreply.github.com>
2021-04-08 15:00:36 +0900

    [fix] request permitall

--- backend/src/main/java/donation/pet/config/security/ResourceServerConfig.java
+++ backend/src/main/java/donation/pet/config/security/ResourceServerConfig.java
@@ -49,7 +49,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
                 // "/blockchain/**", "/chats/**", "/kakao-pay/**", "/adopts/**"
                     .anyRequest()
-                        .authenticated()
+                        .permitAll()
                     .and()
 
                 .exceptionHandling()
